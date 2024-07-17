eeab919e917d394faee37e4e2f157250452c242b
smallpop <wtranviet@proton.me>
2024-07-16 21:21:57 -0400

    add new token, update lex test

--- include/Lex/Lex.hpp
+++ include/Lex/Lex.hpp
@@ -21,47 +21,47 @@ class Token {
 
 private:
   llvm::StringRef Lexeme; // string ref because we need the pointer to the
-                          // location in the memory buffer
+  // location in the memory buffer
   Basic::tok::Tag LexicalTag;
   Token(llvm::StringRef Lexeme, Basic::tok::Tag LexicalTag);
 
 public:
   Basic::tok::Tag getTag() const { return LexicalTag; }
   bool isIdentifier() const {
-    return LexicalTag == Basic::tok::Tag::identifier;
+	return LexicalTag == Basic::tok::Tag::identifier;
   }
   bool isPunctuator() const {
-    return LexicalTag < Basic::tok::Tag::last_punc &&
-           LexicalTag > Basic::tok::Tag::last_tok;
+	return LexicalTag < Basic::tok::Tag::last_punc &&
+		LexicalTag > Basic::tok::Tag::last_tok;
   }
   bool isBaseType() const {
-    return LexicalTag < Basic::tok::Tag::kw_last_type &&
-           LexicalTag > Basic::tok::Tag::last_punc;
+	return LexicalTag < Basic::tok::Tag::kw_last_type &&
+		LexicalTag > Basic::tok::Tag::last_punc;
   };
   llvm::StringRef getIdentifier() {
-    assert(LexicalTag == Basic::tok::identifier &&
-           "Cannot get identifier of non-identifier token.");
-    return Lexeme;
+	assert(LexicalTag == Basic::tok::identifier &&
+		"Cannot get identifier of non-identifier token.");
+	return Lexeme;
   }
 
   llvm::StringRef getLexeme() { return Lexeme; }
 
   void prettyPrint() {
-    std::cout << Basic::tok::getTokenName(LexicalTag) << ": " << Lexeme.str()
-              << std::endl;
+	std::cout << Basic::tok::getTokenName(LexicalTag) << ": " << Lexeme.str()
+			  << std::endl;
   }
 
   llvm::SMLoc getLoc() { return llvm::SMLoc::getFromPointer(Lexeme.data()); }
 
   llvm::SMLoc getRightmostLoc() {
-    return llvm::SMLoc::getFromPointer(Lexeme.data() + Lexeme.size() - 1);
+	return llvm::SMLoc::getFromPointer(Lexeme.data() + Lexeme.size() - 1);
   }
 
   llvm::SMRange getLocRange() {
-    llvm::SMLoc End =
-        llvm::SMLoc::getFromPointer(Lexeme.data() + Lexeme.size());
-    assert(End.isValid());
-    return {llvm::SMLoc::getFromPointer(Lexeme.data()), End};
+	llvm::SMLoc End =
+		llvm::SMLoc::getFromPointer(Lexeme.data() + Lexeme.size());
+	assert(End.isValid());
+	return {llvm::SMLoc::getFromPointer(Lexeme.data()), End};
   }
 
   bool is(Basic::tok::Tag K) const { return K == LexicalTag; }
@@ -80,13 +80,13 @@ private:
   Token formErr();
 
   void addKeyword(const std::string &Kw, Basic::tok::Tag Tag) {
-    keywordMap.insert(std::make_pair(Kw, Tag));
+	keywordMap.insert(std::make_pair(Kw, Tag));
   }
 
   Basic::tok::Tag findKeyword(std::string_view Name);
   std::vector<Token> tokens; // keep track of old tokens for error messages
   std::deque<Token>
-      unconsumed; // unconsumed tokens that are stored if we looked ahead.
+	  unconsumed; // unconsumed tokens that are stored if we looked ahead.
   Token lexString();
   Token lexNum();
   Token lexIdentifier();
@@ -102,23 +102,22 @@ private:
 
 public:
   Lexer(const std::shared_ptr<llvm::SourceMgr> &SrcMgr, DiagEngine &Diags)
-      : srcManager(SrcMgr), diagnostics(Diags) {
-    currentBuffer = SrcMgr->getMainFileID();
-    curBuf = SrcMgr->getMemoryBuffer(currentBuffer)->getBuffer();
-    bufPtr = curBuf.begin();
-    addKeywords();
+	  : srcManager(SrcMgr), diagnostics(Diags) {
+	currentBuffer = SrcMgr->getMainFileID();
+	curBuf = SrcMgr->getMemoryBuffer(currentBuffer)->getBuffer();
+	bufPtr = curBuf.begin();
+	addKeywords();
   }
 
   llvm::SMLoc getCurLoc() { return llvm::SMLoc::getFromPointer(bufPtr); }
 
   static llvm::SMLoc getLocFrom(const char *Ptr) {
-    return llvm::SMLoc::getFromPointer(Ptr - 1);
+	return llvm::SMLoc::getFromPointer(Ptr - 1);
   }
 
   Token &advance();
   Token &peek(); // equivalent to lookahead(1)
   Token previous();
-  ;
   Token lookahead(size_t HowMuch);
   bool atEnd();
 };
