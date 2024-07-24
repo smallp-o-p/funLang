d2f3040d7f7d4cd6293c207a5c241774542c8e78
smallpop <wtranviet@proton.me>
2024-07-23 20:12:51 -0400

    update tokens to now reference an identifier table for identifiers

--- include/Lex/Lex.hpp
+++ include/Lex/Lex.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "Basic/Basic.hpp"
 #include "Basic/Diag.hpp"
+#include "Basic/IdentifierTable.hpp"
 #include "llvm/ADT/StringMap.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/FormatVariadicDetails.h"
+#include "llvm/ADT/StringSet.h"
 #include <cassert>
 #include <cstdint>
 #include <deque>
@@ -16,93 +18,142 @@
 #include <sys/types.h>
 #include <vector>
 
+namespace funLang {
+
 class Token {
   friend class Lexer;
-
 private:
-  llvm::StringRef Lexeme; // string ref because we need the pointer to the
-  // location in the memory buffer
   Basic::tok::Tag LexicalTag;
-  Token(llvm::StringRef Lexeme, Basic::tok::Tag LexicalTag);
+  void *TheData;
+  size_t DataSize;
+
+  llvm::SMLoc LocStart;
+  llvm::SMLoc LocEnd;
+
+  // error
+  Token(llvm::SMLoc ErrLoc)
+	  : TheData(nullptr), LocStart(ErrLoc), LocEnd(llvm::SMLoc()), LexicalTag(Basic::tok::err), DataSize(0) {}
+  // literal
+  Token(llvm::StringRef Lexeme, size_t Size, Basic::tok::Tag LexicalTag, llvm::SMLoc Start, llvm::SMLoc End)
+	  : LexicalTag(LexicalTag), LocStart(Start), LocEnd(End), TheData((void *)Lexeme.data()), DataSize(Size) {};
+  // punctuator or keyword
+  Token(Basic::tok::Tag Kind, llvm::SMLoc LocStart, llvm::SMLoc LocEnd, size_t Size = 1)
+	  : LexicalTag(Kind), LocStart(LocStart),
+		LocEnd(LocEnd),
+		TheData(nullptr), DataSize(Size) {}
+  // identifier
+  Token(Basic::tok::Tag LexicalTag,
+		llvm::StringMapEntry<std::nullopt_t> *IdentifierTableEntry,
+		llvm::SMLoc LocStart,
+		llvm::SMLoc LocEnd,
+		size_t Size)
+	  : LexicalTag(LexicalTag), LocStart(LocStart), LocEnd(LocEnd), TheData((void *)IdentifierTableEntry),
+		DataSize(Size) {};
 
 public:
   Basic::tok::Tag getTag() const { return LexicalTag; }
-  bool isIdentifier() const {
+
+  llvm::StringRef getLexeme() const;
+
+  llvm::StringRef getLiteral() const {
+	assert(isLiteral() && "Not a literal!");
+	return {reinterpret_cast<const char *>(TheData), DataSize};
+  }
+
+  llvm::StringRef getIdentifier() const {
+	assert(isIdentifier() && "Not an identifier!");
+	return static_cast<llvm::StringMapEntry<std::nullopt_t> *>(TheData)->first();
+  }
+  llvm::StringMapEntry<std::nullopt_t> *getIdentifierTableEntry() const {
+	assert(isIdentifier() && "Not an identifier!");
+	return static_cast<llvm::StringMapEntry<std::nullopt_t> *>(TheData);
+  }
+
+  inline bool isIdentifier() const {
 	return LexicalTag == Basic::tok::Tag::identifier;
   }
+
   bool isPunctuator() const {
 	return LexicalTag < Basic::tok::Tag::last_punc &&
 		LexicalTag > Basic::tok::Tag::last_tok;
   }
+
+  bool isKeyword() const {
+	return LexicalTag > Basic::tok::last_punc && LexicalTag < Basic::tok::kw_last_keyword;
+  }
+
+  bool isLiteral() const {
+	return LexicalTag == Basic::tok::floating_constant || LexicalTag == Basic::tok::numeric_constant
+		|| LexicalTag == Basic::tok::string_literal;
+  }
+
+  static bool isLiteral(Basic::tok::Tag Kind) {
+	return Kind == Basic::tok::floating_constant || Kind == Basic::tok::numeric_constant
+		|| Kind == Basic::tok::string_literal;
+  }
+
   bool isBaseType() const {
 	return LexicalTag < Basic::tok::Tag::kw_last_type &&
 		LexicalTag > Basic::tok::Tag::last_punc;
   };
-  llvm::StringRef getIdentifier() {
-	assert(LexicalTag == Basic::tok::identifier &&
-		"Cannot get identifier of non-identifier token.");
-	return Lexeme;
-  }
-
-  llvm::StringRef getLexeme() { return Lexeme; }
 
   void prettyPrint() {
-	std::cout << Basic::tok::getTokenName(LexicalTag) << ": " << Lexeme.str()
-			  << std::endl;
+	std::cout << Basic::tok::getTokenName(LexicalTag) << ": ";
+	if (isIdentifier()) {
+	  std::cout << getIdentifier().str();
+	} else if (isLiteral()) {
+	  std::cout << getLiteral().str();
+	} else if (isKeyword()) {
+	  std::cout << Basic::tok::getKeywordSpelling(LexicalTag);
+	} else if (isPunctuator()) {
+	  std::cout << Basic::tok::getPunctuatorSpelling(LexicalTag);
+	}
   }
 
-  llvm::SMLoc getLoc() { return llvm::SMLoc::getFromPointer(Lexeme.data()); }
+  llvm::SMLoc getLoc() { return LocStart; }
 
   llvm::SMLoc getRightmostLoc() {
-	return llvm::SMLoc::getFromPointer(Lexeme.data() + Lexeme.size() - 1);
+	return LocEnd;
   }
 
   llvm::SMRange getLocRange() {
-	llvm::SMLoc End =
-		llvm::SMLoc::getFromPointer(Lexeme.data() + Lexeme.size());
-	assert(End.isValid());
-	return {llvm::SMLoc::getFromPointer(Lexeme.data()), End};
+	return {LocStart, LocEnd};
   }
 
   bool is(Basic::tok::Tag K) const { return K == LexicalTag; }
 };
 
 class Lexer {
+  friend class funLang::IdentifierTable;
 private:
-  llvm::StringMap<Basic::tok::Tag> keywordMap;
+  std::vector<Token> tokens; // keep track of old tokens for error messages
+  std::deque<Token> unconsumed; // unconsumed tokens that are stored if we looked ahead.
+  llvm::StringMap<Basic::tok::Tag> KeywordTable;
+
+  std::shared_ptr<llvm::SourceMgr> srcManager;
+  DiagEngine diagnostics;
+  llvm::StringRef curBuf;
+  llvm::StringRef::iterator bufPtr;
+  funLang::IdentifierTable IdentTable;
+  uint32_t currentBuffer = 0;
+
   void addKeywords() {
 #define KEYWORD(NAME, FLAGS) addKeyword(#NAME, Basic::tok::kw_##NAME);
 #include "Basic/defs/TokenTags.def"
   }
 
   Token formToken(const char *TokEnd, Basic::tok::Tag Kind);
-
   Token formErr();
-
-  void addKeyword(const std::string &Kw, Basic::tok::Tag Tag) {
-	keywordMap.insert(std::make_pair(Kw, Tag));
-  }
-
-  Basic::tok::Tag findKeyword(std::string_view Name);
-  std::vector<Token> tokens; // keep track of old tokens for error messages
-  std::deque<Token>
-	  unconsumed; // unconsumed tokens that are stored if we looked ahead.
+  void addKeyword(const std::string &Kw, Basic::tok::Tag Tag) { KeywordTable.insert(std::make_pair(Kw, Tag)); }
+  Basic::tok::Tag findKeyword(llvm::StringRef Name);
   Token lexString();
   Token lexNum();
   Token lexIdentifier();
   Token getNext();
   bool nextIs(char C);
-
-private:
-  std::shared_ptr<llvm::SourceMgr> srcManager;
-  DiagEngine diagnostics;
-  llvm::StringRef curBuf;
-  llvm::StringRef::iterator bufPtr;
-  uint32_t currentBuffer = 0;
-
 public:
   Lexer(const std::shared_ptr<llvm::SourceMgr> &SrcMgr, DiagEngine &Diags)
-	  : srcManager(SrcMgr), diagnostics(Diags) {
+	  : srcManager(SrcMgr), diagnostics(Diags), IdentTable(funLang::IdentifierTable()) {
 	currentBuffer = SrcMgr->getMainFileID();
 	curBuf = SrcMgr->getMemoryBuffer(currentBuffer)->getBuffer();
 	bufPtr = curBuf.begin();
@@ -121,3 +172,5 @@ public:
   Token lookahead(size_t HowMuch);
   bool atEnd();
 };
+
+}
--- tests/Lex/LexTests.cpp
+++ tests/Lex/LexTests.cpp
@@ -1,35 +1,43 @@
 #include "Lex/Lex.hpp"
-#include "../../include/Basic/Basic.hpp"
 #include "gtest/gtest.h"
 #include <llvm/Support/InitLLVM.h>
 #include <llvm/Support/SMLoc.h>
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/raw_ostream.h>
 #include <filesystem>
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 
-std::unique_ptr<Lexer> makeLexer(std::string filename) {
+using namespace funLang;
+using namespace Basic;
 
-  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> fileOrErr =
-	  llvm::MemoryBuffer::getFile(filename);
-  if (std::error_code bufferErr = fileOrErr.getError()) {
-	std::cout << bufferErr.message() << " " << bufferErr.value();
-	return nullptr;
+class LexTests : public testing::Test {
+protected:
+  Lexer *LexerObj;
+  std::shared_ptr<DiagEngine> Diags;
+  std::shared_ptr<llvm::SourceMgr> SrcMgr;
+  bool setBuffer(llvm::StringRef Buf, bool Str = false) {
+
+	llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> FileOrError = nullptr;
+	if (Str) {
+	  FileOrError = llvm::MemoryBuffer::getMemBufferCopy(Buf);
+	} else {
+	  FileOrError = llvm::MemoryBuffer::getFile(Buf);
+	}
+	if (std::error_code BufferErr = FileOrError.getError()) {
+	  std::cout << BufferErr.message() << " " << BufferErr.value();
+	  return false;
+	}
+	SrcMgr->AddNewSourceBuffer(std::move(*FileOrError), llvm::SMLoc());
+	Diags = std::make_shared<DiagEngine>(SrcMgr);
+	LexerObj = new Lexer(SrcMgr, *Diags);
+	return true;
+
   }
-  auto srcMgr = std::make_shared<llvm::SourceMgr>();
-  srcMgr->AddNewSourceBuffer(std::move(*fileOrErr), llvm::SMLoc());
-  DiagEngine diag = DiagEngine(srcMgr);
-  return std::make_unique<Lexer>(srcMgr, diag);
-}
+  LexTests()
+	  : SrcMgr(std::make_shared<llvm::SourceMgr>()), Diags(nullptr), LexerObj(nullptr) {
+  }
+};
 
-TEST(LexTests, BuildLexerObject) {
-  using namespace Basic;
-  auto lexx = makeLexer("./lex_Basic.txt");
-
-  EXPECT_NE(nullptr, lexx);
-}
 
 // file should get copied over to executable directory
 //+-*/>< =(){}!,:;
@@ -37,11 +45,8 @@ TEST(LexTests, BuildLexerObject) {
 //>=
 //==
 //!=
-TEST(LexTests, LexBasicToken) {
-  using namespace Basic;
-
-  auto lexer = makeLexer("./lex_Basic.txt");
-  EXPECT_NE(nullptr, lexer) << "Lexer returned nullptr";
+TEST_F(LexTests, LexBasicTokens) {
+  ASSERT_TRUE(setBuffer("./lex_Basic.txt"));
   std::vector<Basic::tok::Tag> expected = {
 	  tok::plus, tok::minus, tok::star, tok::slash,
 	  tok::greater, tok::less, tok::equal, tok::l_paren,
@@ -50,75 +55,69 @@ TEST(LexTests, LexBasicToken) {
 	  tok::greaterequal, tok::equalequal, tok::exclaimequal, tok::plusequal,
 	  tok::minusequal, tok::starequal, tok::slashequal};
   for (auto tok : expected) {
-	Token lexed_token = lexer->advance();
+	Token lexed_token = LexerObj->advance();
 	EXPECT_EQ(lexed_token.getTag(), tok)
 			<< "Expected: " << tok::getTokenName(tok)
 			<< " Received: " << tok::getTokenName(lexed_token.getTag());
   }
-  EXPECT_EQ(lexer->advance().getTag(), tok::eof);
+  EXPECT_EQ(LexerObj->advance().getTag(), tok::eof);
 }
 
-TEST(LexTests, LexKeywords) {
-  using namespace Basic;
-  auto lexer = makeLexer("./lex_Keywords.txt");
+TEST_F(LexTests, LexKeywords) {
   std::vector<Basic::tok::Tag> expected = {
 	  tok::kw_void, tok::kw_bool, tok::kw_char, tok::kw_string,
 	  tok::kw_i32, tok::kw_i64, tok::kw_f32, tok::kw_f64,
 	  tok::kw_return, tok::kw_true, tok::kw_false, tok::kw_for,
 	  tok::kw_while, tok::kw_loop, tok::kw_struct, tok::kw_enum};
+  ASSERT_TRUE(setBuffer("./lex_Keywords.txt"));
   for (auto expected_tok : expected) {
-	Token lexed_token = lexer->advance();
+	Token lexed_token = LexerObj->advance();
 	EXPECT_EQ(lexed_token.getTag(), expected_tok)
 			<< "Expected: " << tok::getTokenName(expected_tok)
 			<< " Received: " << tok::getTokenName(lexed_token.getTag());
   }
 }
 
-TEST(LexTests, IntNumLiterals) {
-  using namespace Basic;
-  auto lexer = makeLexer("./lex_IntNumLits.txt");
-  Token tok = lexer->advance();
+TEST_F(LexTests, IntNumLiterals) {
+  ASSERT_TRUE(setBuffer("./lex_IntNumLits.txt"));
+  Token tok = LexerObj->advance();
   while (tok.getTag() != tok::eof) {
 	EXPECT_EQ(tok.getTag(), tok::numeric_constant)
 			<< "\nExpected: " << tok::getTokenName(tok::numeric_constant)
 			<< " Received: " << tok::getTokenName(tok.getTag());
-	tok = lexer->advance();
+	tok = LexerObj->advance();
   }
+
   EXPECT_EQ(tok.getTag(), tok::eof);
 }
 
-TEST(LexTests, RealNumLiterals) {
-  using namespace Basic;
-  auto lexer = makeLexer("./lex_RealNumLits.txt");
-  Token tok = lexer->advance();
+TEST_F(LexTests, RealNumLiterals) {
+  ASSERT_TRUE(setBuffer("./lex_RealNumLits.txt"));
+  Token tok = LexerObj->advance();
   while (tok.getTag() != tok::eof) {
 	EXPECT_EQ(tok.getTag(), tok::floating_constant)
 			<< "\nExpected: " << tok::getTokenName(tok::floating_constant)
 			<< " Received: " << tok::getTokenName(tok.getTag());
-	tok = lexer->advance();
+	tok = LexerObj->advance();
   }
   EXPECT_EQ(tok.getTag(), tok::eof);
 }
 
-TEST(LexTests, StringLits) {
-  using namespace Basic;
-  std::unique_ptr<Lexer> lex = std::move(makeLexer("./lex_StringLits.txt"));
-  EXPECT_NE(nullptr, lex);
+TEST_F(LexTests, StringLits) {
+  ASSERT_TRUE(setBuffer("./lex_StringLits.txt"));
 
-  Token tok = lex->advance();
+  Token tok = LexerObj->advance();
   while (tok.getTag() != tok::eof) {
 	EXPECT_EQ(tok::string_literal, tok.getTag())
 			<< "\nExpected: " << tok::getTokenName(tok::string_literal)
 			<< " Received: " << tok::getTokenName(tok.getTag());
-	tok = lex->advance();
+	tok = LexerObj->advance();
   }
-  EXPECT_EQ(tok::eof, lex->previous().getTag());
+  EXPECT_EQ(tok::eof, LexerObj->previous().getTag());
 }
 
-TEST(LexTests, ExampleFunc) {
-  using namespace Basic;
-  auto lex = makeLexer("./lex_ExampleFunc.txt");
-  EXPECT_NE(lex, nullptr) << "Lexer init returned nullptr.";
+TEST_F(LexTests, ExampleFunc) {
+  ASSERT_TRUE(setBuffer("./lex_ExampleFunc.txt"));
   std::vector<Basic::tok::Tag> expected = {
 	  tok::kw_i32, tok::identifier, tok::l_paren,
 	  tok::kw_i32, tok::identifier, tok::comma,
@@ -128,16 +127,14 @@ TEST(LexTests, ExampleFunc) {
 	  tok::kw_return, tok::numeric_constant, tok::semi,
 	  tok::r_brace};
   for (auto expected_tok : expected) {
-	EXPECT_EQ(expected_tok, lex->advance().getTag())
+	EXPECT_EQ(expected_tok, LexerObj->advance().getTag())
 			<< "Mismatch; Expected: " << tok::getTokenName(expected_tok)
-			<< "\nReceived: " << tok::getTokenName(lex->previous().getTag());
+			<< "\nReceived: " << tok::getTokenName(LexerObj->previous().getTag());
   }
 }
 
-TEST(LexTests, WithErrs) {
-  using namespace Basic;
-  auto lexer = makeLexer("./lex_withErrors.txt");
-  EXPECT_NE(lexer, nullptr) << "Lexer init returned nullptr.\n";
+TEST_F(LexTests, WithErrs) {
+  ASSERT_TRUE(setBuffer("./lex_withErrors.txt"));
   std::vector<Basic::tok::Tag> expected = {
 	  tok::err, tok::err, tok::err,
 	  tok::err, tok::err, tok::kw_void,
@@ -146,10 +143,32 @@ TEST(LexTests, WithErrs) {
   };
 
   for (auto expected_tok : expected) {
-	EXPECT_EQ(expected_tok, lexer->advance().getTag())
+	EXPECT_EQ(expected_tok, LexerObj->advance().getTag())
 			<< "Mismatch; Expected: " << tok::getTokenName(expected_tok)
-			<< "\nReceived: " << tok::getTokenName(lexer->previous().getTag());
+			<< "\nReceived: " << tok::getTokenName(LexerObj->previous().getTag());
   }
+}
+
+TEST_F(LexTests, IdentifierEqualityTest) {
+  ASSERT_TRUE(setBuffer("id id", true));
+  auto id = LexerObj->advance();
+  auto id2 = LexerObj->advance();
+
+  ASSERT_EQ(id.getIdentifier(), id2.getIdentifier());
+  ASSERT_EQ(id.getIdentifierTableEntry(), id2.getIdentifierTableEntry());
+}
+
+TEST_F(LexTests, IdentifierInequalityTest) {
+  ASSERT_TRUE(setBuffer("one id two id one", true));
+  auto one = LexerObj->advance();
+  auto id = LexerObj->advance();
+  auto two = LexerObj->advance();
+  auto id2 = LexerObj->advance();
+  auto one2 = LexerObj->advance();
+  ASSERT_NE(one.getIdentifierTableEntry(), two.getIdentifierTableEntry());
+  ASSERT_NE(id.getIdentifierTableEntry(), one.getIdentifierTableEntry());
+  ASSERT_NE(id2.getIdentifierTableEntry(), two.getIdentifierTableEntry());
+  ASSERT_EQ(one.getIdentifierTableEntry(), one2.getIdentifierTableEntry());
 }
 
 int main() {
